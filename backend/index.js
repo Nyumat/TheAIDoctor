46da2bdc3a2d9d4d12dbbaada392ff4ed99d6ac7
Thomas <nyumat18@gmail.com>
2023-01-13 01:45:45 -0800

    Added Tailwind and Backend Web Server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// Main route
+app.get("/", (req, res) => {
+      res.status(200).json({ data: "Hello Worldd" });
+});
+
+app.listen(process.env.PORT, () => {
+      console.log(`\n\n Server running on port ${process.env.PORT} \n\n`);
+});
+// Connect to MongoDB (TBD)
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pallete-purple': {
+          '50': '#eeefff',
+          '100': '#e1e2fe',
+          '200': '#c8c9fd',
+          '300': '#a8a7fa',
+          '400': '#8d83f6',
+          '500': '#806aef',
+          '600': '#6e49e2',
+          '700': '#5f3bc7',
+          '800': '#4e32a1',
+          '900': '#412f80',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
