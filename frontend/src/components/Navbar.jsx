6e23f7f278d237c8e7a9f66d7812577b8c291b9b
Thomas <nyumat18@gmail.com>
2023-01-13 22:50:16 -0800

    Added Firebase, RealTime Chat

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,31 @@
 import React from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+
+
+function SignOut() {
+  const auth = firebase.auth();
+  const navigate = useNavigate();
+  return (
+    auth.currentUser && (
+      <button
+        className="text2xl text-pallete-purple-600 hover:text-pallete-purple-800"
+        onClick={() => {
+          auth.signOut();
+          navigate("/");
+        }}
+      >
+        Sign Out
+      </button>
+    )
+  );
+}
 
 const Navbar = () => {
   return (
@@ -6,78 +33,14 @@ const Navbar = () => {
       {/* <!-- Navbar --> */}
       <nav className="navbar navbar-expand-lg shadow-md py-2 bg-pallete-purple-200 relative flex items-center w-full justify-between">
         <div className="px-6 w-full flex flex-wrap items-center justify-between">
-          <div className="flex items-center">
-            <button
-              className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContentY"
-              aria-controls="navbarSupportedContentY"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                className="w-5"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            className="navbar-collapse collapse grow items-center"
-            id="navbarSupportedContentY"
-          >
-            <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
-              <li className="nav-item">
-                <a
-                  className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-                  href="#!"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-                  href="#!"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-                  href="#!"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2 lg:mb-0">
-                <a
-                  className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-                  href="#!"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  About
-                </a>
-              </li>
-            </ul>
+          <div className="w-full relative flex flex-col justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+            <div className="absolute right-0 pr-16 mt-4">
+              <SignOut />
+            </div>
+            <h1 className="text-white text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase">
+              The AI Doctor
+            </h1>
+            {/* Todo, routers */}
           </div>
         </div>
       </nav>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,19 @@ import { useNavigate } from "react-router";
 import requests from "../utils/requests";
 import axios from "axios";
 import Layout from "../components/Layout";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const STATIC_MESSAGES = [
+  { id: 1, text: "Hello, how are you?", agent: "user" },
+  { id: 2, text: "I am fine, how are you?", agent: "bot" },
+  { id: 3, text: "I am fine too, thanks for asking.", agent: "user" },
+  { id: 4, text: "You are welcome." },
+];
+
+const auth = firebase.auth();
 
 const Home = () => {
   const navigate = useNavigate();
@@ -10,9 +23,24 @@ const Home = () => {
   const [active, setActive] = useState(false);
   const [query, setQuery] = useState("");
 
+  const [user] = useAuthState(auth);
+
+  // if (user) {
+  //   console.log(JSON.stringify(user.displayName));
+  // }
+
+  const [messages, setMessages] = useState(STATIC_MESSAGES);
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   await fetchChatGPTResponse(query);
+  // };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await fetchChatGPTResponse(query);
+    setMessages([...messages, { id: 5, text: query, agent: "user" }]);
+
+    setQuery("");
   };
 
   const fetchChatGPTResponse = async (query) => {
@@ -42,27 +70,41 @@ const Home = () => {
   return (
     <>
       <Layout>
-        <div className="flex flex-col items-center justify-center h-full gap-8 py-16">
-          <h1 className="text-2xl text-pallete-purple-900">Home Page</h1>
-          <button
-            onClick={() => navigate("/")}
-            className="btn btn-primary bg-pallete-purple-500"
-          >
-            Go to Intro
-          </button>
-
-          <form
-            type="submit"
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-4"
-          >
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered input-primary input-lg w-full max-w-xs"
-              onChange={(e) => setQuery(e.target.value)}
-            />
-          </form>
+        <div className="flex flex-col items-center justify-center mt-16 h-[40rem] gap-8 py-16 bg-gray-600 w-[64rem] overflow-scroll">
+          {messages.map((message, index) => (
+            <div>
+              <div className="flex flex-row gap-4" key={index}>
+                {message.agent === "user" ? (
+                  <div className="flex flex-row gap-4 bg-green-700">
+                    {message.text}
+                  </div>
+                ) : (
+                  <div className="flex flex-row gap-4 bg-blue-700">
+                    {message.text}
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+          {/* This cannot collide. */}
+          <div className="translate-y-48 pt-32 fixed">
+            <div className="flex flex-row gap-4 w-full">
+              <div className="flex flex-row gap-4">
+                <textarea
+                  className="textarea textarea-primary"
+                  onChange={(e) => setQuery(e.target.value)}
+                  placeholder="Type Your Message Here"
+                ></textarea>
+                <button
+                  type="submit"
+                  onClick={handleSubmit}
+                  className="btn btn-primary bg-pallete-purple-500"
+                >
+                  Send
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </Layout>
     </>
