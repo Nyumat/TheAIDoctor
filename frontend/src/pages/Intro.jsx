e7cae07a8f9f106e107c7ec04822c47aa1d761bf
Jeff H <82061589+solderq35@users.noreply.github.com>
2023-01-13 21:48:01 -0800

    intro pages ui changes (#4)

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -12,6 +12,7 @@ const Intro = () => {
       >
         Get Started
       </button>
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Doctor AI</h1>
       {/* 
       <div class="absolute top-0 right-0 ...">
       <button
@@ -33,18 +34,20 @@ const Intro = () => {
           class="w-full"
           alt="Sample image"
         />
-        
       </div>
-      <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+      
+      <div class="">
         <form>
           <div class="flex flex-row items-center justify-center lg:justify-start">
-            <p class="text-lg mb-0 mr-1">Sign in with</p>
-
             <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+      >
+        Sign in with
+
+<br></br>
+        <span
               type="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+
             >
               {/* Google */}
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" class="w-4 h-4">
@@ -54,65 +57,14 @@ const Intro = () => {
                   d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
                 />
               </svg>
-            </button>
+            </span>
+      </button>
 
-          </div>
 
-          <div
-            class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-          >
-            <p class="text-center font-semibold mx-4 mb-0">Or</p>
-          </div>
 
-          {/* Email input */}
-          <div class="mb-6">
-            <input
-              type="text"
-              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="exampleFormControlInput2"
-              placeholder="Email address"
-            />
-          </div>
-
-          {/* Password input */}
-          <div class="mb-6">
-            <input
-              type="password"
-              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="exampleFormControlInput2"
-              placeholder="Password"
-            />
-          </div>
-
-          <div class="flex justify-between items-center mb-6">
-            <div class="form-group form-check">
-              <input
-                type="checkbox"
-                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                id="exampleCheck2"
-              />
-              <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
-                >Remember me</label
-              >
-            </div>
-            <a href="#!" class="text-gray-800">Forgot password?</a>
           </div>
 
           <div class="text-center lg:text-left">
-            <button
-              type="button"
-              class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Login
-            </button>
-            <p class="text-sm font-semibold mt-2 pt-1 mb-0">
-              Don't have an account?
-              <a
-                href="#!"
-                class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                > Register</a
-              >
-            </p>
           </div>
         </form>
       </div>
