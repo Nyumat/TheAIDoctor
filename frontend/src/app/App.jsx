d421f821f8135037fc16ef7e421dd6d711467837
Thomas <nyumat18@gmail.com>
2023-01-15 12:54:07 -0800

    Chat Window Styling Improv

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -7,15 +7,22 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import Settings from "../pages/Settings";
 
 import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const auth = firebase.auth();
 
 function App() {
   const queryClient = new QueryClient();
+  const [user] = useAuthState(auth);
   return (
     <div className="flex flex-col bg-pallete-purple-300 h-screen w-screen items-center justify-center">
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/home" element={<Home userPhotoUrl={user ? user.photoURL : "https://picsum.photos/200"} />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/" element={<Intro />} />
           </Routes>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,8 +9,8 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import { TypeAnimation } from "react-type-animation";
 import SendIcon from "../components/SendIcon";
-
-
+import RobotDoctor from "../assets/robot_doctor.jpg";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const STATIC_MESSAGES = [{ id: 1, text: "Hello, how are you?", agent: "bot" }];
 
@@ -18,7 +18,7 @@ const STATIC_MESSAGES = [{ id: 1, text: "Hello, how are you?", agent: "bot" }];
 
 const auth = firebase.auth();
 
-const Home = () => {
+const Home = ({ userPhotoUrl }) => {
   const navigate = useNavigate();
   const [resData, setResData] = useState([]);
   const [active, setActive] = useState(false);
@@ -31,6 +31,8 @@ const Home = () => {
   // }
 
   const [messages, setMessages] = useState([]);
+
+  const [parent] = useAutoAnimate();
 
   // const handleSubmit = async (e) => {
   //   e.preventDefault();
@@ -54,14 +56,17 @@ const Home = () => {
       setI(i + 1);
       let text = query;
       setQuery("");
-      await getDoctorResponse(text, old_messages);
+      setTimeout(async () => {
+        await getDoctorResponse(text, old_messages);
+      }, 1000);
     }
   };
 
   const getDoctorResponse = async (query, old_messages) => {
     if (query === "") return;
     // const response = await fetchChatGPTResponse(query);
-    const response = "Hello, how are you?";
+    const response =
+      "Hello, how are you? I'm fine, thank you. The weather today is cloud.";
     setMessages([...old_messages, { id: i, text: response, agent: "bot" }]);
     setI(i + 1);
   };
@@ -99,24 +104,38 @@ const Home = () => {
       <Layout>
         <div
           id="chat-window"
-          className="flex flex-col items-center h-[30rem] justify-center gap-8 py-16 bg-white w-[64rem] rounded-2xl scrollbar-hide overflow-scroll"
+          className="flex flex-col items-center h-[30rem] justify-center gap-8 py-16 bg-pallete-purple-100 w-[64rem] rounded-2xl scrollbar-hide overflow-scroll"
         >
           <h1 className="text-pallete-purple-500 text-2xl mb-64 relative">
             Welcome to AI Doctor Chat{" "}
           </h1>
-          <div className="w-full px-8 ">
+          <div className="w-full px-8" ref={parent}>
             {messages.map((message, index) => (
-              <div className="flex flex-col" key={index}>
+              <div className="flex flex-col pt-6" key={index}>
                 {message.agent === "user" ? (
-                  <div className="chat chat-end ">
-                    <div className="chat-bubble chat-bubble-info text-white">
-                      {message.text}
+                  <div className="flex flex-row justify-end gap-4">
+                    <div className="chat chat-end">
+                      <div className="chat-image avatar">
+                        <div className="w-10 rounded-full">
+                          <img src={userPhotoUrl} />
+                        </div>
+                      </div>
+                      <div className="chat-bubble bg-pallete-purple-300 text-gray-900">
+                        {message.text}
+                      </div>
                     </div>
                   </div>
                 ) : (
-                  <div className="chat chat-start">
-                    <div className="chat-bubble chat-bubble-primary">
-                      <TypeAnimation sequence={[message.text]} />
+                  <div className="flex flex-row justify-start gap-4">
+                    <div className="chat chat-start w-1/2">
+                      <div className="chat-image avatar">
+                        <div className="w-10 rounded-full">
+                          <img src={RobotDoctor} />
+                        </div>
+                      </div>
+                      <div className="chat-bubble text-white bg-pallete-purple-700 whitespace-normal break-words">
+                        <TypeAnimation sequence={[message.text]} />
+                      </div>
                     </div>
                   </div>
                 )}
@@ -124,11 +143,11 @@ const Home = () => {
             ))}
           </div>
 
-          <div className= "absolute translate-x-52 translate-y-72 flex flex-col justify-center items-center margin">
+          <div className="absolute translate-x-52 translate-y-72 flex flex-col justify-center items-center margin">
             <div className=" flex flex-row gap-4 w-full">
               <div className="flex flex-row gap-4">
                 <textarea
-                  className="textarea textarea-primary resize-none w-[64rem] h-[5rem]"
+                  className="textarea textarea-primary resize-none w-[64rem] h-[5rem] bg-pallete-purple-100 rounded-2xl text-gray-900"
                   // value = {(e) => setQuery(e.target.value)}
                   onChange={(e) => setQuery(e.target.value)}
                   onKeyDown={(e) => handleSubmitKeyboard(i, e)}
