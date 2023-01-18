11bd0c151b38d6818c701c135b0e0cbca8fbfb8f
Thomas <nyumat18@gmail.com>
2023-01-18 01:04:25 -0800

    Responsive Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function SignOut() {
   return (
     auth.currentUser && (
       <LogoutIcon
-        className="text2xl text-pallete-purple-600 hover:text-pallete-purple-800"
+        className="text-xl ml-auto text-pallete-purple-600 hover:text-pallete-purple-800"
         onClick={async () => {
           await auth.signOut();
         }}
@@ -40,30 +40,24 @@ const Navbar = () => {
   return (
     <header className="absolute top-0 w-screen">
       {/* <!-- Navbar --> */}
-      <nav className="navbar navbar-expand-lg shadow-md py-2 bg-pallete-purple-200 relative flex items-center w-full justify-between">
-        <div className="px-6 w-full flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex flex-col justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
-            <div className="absolute right-0 pr-16 mt-2 ml-6">
-              <Link to="/" className="text-white text-xl font-bold">
-                <SignOut />
-              </Link>
-            </div>
-            <Link
-              to="/"
-              className="text-white text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              The AI Doctor
+      <nav className="navbar h-fit navbar-expand-lg shadow-md py-2 bg-pallete-purple-200 relative flex items-center w-full justify-between">
+        <div className="px-6 w-full">
+          <Link
+            to="/"
+            className="text-white text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+          >
+            The AI Doctor
+          </Link>
+          <div className="flex flex-row gap-4 w-full flex-wrap justify-end align-center items-center">
+            <Link to="/" className="text-white font-bold">
+              <SignOut className="cursor-pointer" />
             </Link>
-            <div className="absolute right-0 mr-36 translate-y-[-43px]">
-              <Link to="/settings" className="text-white text-xl font-bold">
-                <SettingsIcon className="cursor-pointer scale-150" />
-              </Link>
-            </div>
-            <div className="absolute right-16 mr-36 translate-y-[-43px] translate-x-[-5px]">
-              <Link to="/home" className="text-white text-xl font-bold">
-                <ChatIcon className="cursor-pointer scale-150" />
-              </Link>
-            </div>
+            <Link to="/settings" className="text-white text-2xl font-bold">
+              <SettingsIcon className="cursor-pointer scale-125 mr-2" />
+            </Link>
+            <Link to="/home" className="text-white text-2xl font-bold">
+              <ChatIcon className="cursor-pointer scale-125" />
+            </Link>
           </div>
         </div>
       </nav>
